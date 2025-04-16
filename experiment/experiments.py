54e8914d78e4741c77048f687d9c2f1a689157fc
Pi Marie <135617413+Pi-Marie@users.noreply.github.com>
2025-04-16 10:26:45 +0200

    experiments

--- experiment/experiments.py
+++ experiment/experiments.py
@@ -146,14 +146,13 @@ def tuning():
              #'mu': 99,
              #'nu': 51,
              #'sinkhorn_reg': 0.002,
-             'sinkhorn_method': SinkhornMethod.STANDARD,
-             #'scaling': ScalingEnums.COLLECTIVE_ROBUST_NORMALIZATION,
-             #'pca': PCAEnums.NO_PCA,
+             #'sinkhorn_method': SinkhornMethod.STANDARD,
+             'scaling': ScalingEnums.COLLECTIVE_ROBUST_NORMALIZATION,
+             #'pca': PCAEnums.FOUR_COMPONENTS,
              }
             for x in [[FeatureEnums.DEG] #, [FeatureEnums.CLUSTER] #, [FeatureEnums.AVG_EGO_DEG], [FeatureEnums.AVG_EGO_CLUSTER], [FeatureEnums.EGO_EDGES], [FeatureEnums.EGO_OUT_EDGES], [FeatureEnums.EGO_NEIGHBORS],  # NetSimile
                       #[FeatureEnums.SUM_EGO_DEG], [FeatureEnums.STD_EGO_DEG], [FeatureEnums.MODE_EGO_DEGS], [FeatureEnums.MEDIAN_EGO_DEGS], [FeatureEnums.MIN_EGO_DEGS], [FeatureEnums.MAX_EGO_DEGS], [FeatureEnums.RANGE_EGO_DEGS], [FeatureEnums.SKEWNESS_EGO_DEGS], [FeatureEnums.KURTOSIS_EGO_DEGS],  # Statistical features on degrees
                       #[FeatureEnums.SUM_EGO_CLUSTER], [FeatureEnums.STD_EGO_CLUSTER], [FeatureEnums.RANGE_EGO_CLUSTER], [FeatureEnums.MIN_EGO_CLUSTER], [FeatureEnums.MAX_EGO_CLUSTER], [FeatureEnums.MEDIAN_EGO_CLUSTER], [FeatureEnums.SKEWNESS_EGO_CLUSTER], [FeatureEnums.KURTOSIS_EGO_CLUSTER], # Augmented clustering features
-                      #[FeatureEnums.SUM_EGO_DEG], [FeatureEnums.STD_EGO_DEG], [FeatureEnums.MODE_EGO_DEGS], [FeatureEnums.MEDIAN_EGO_DEGS], [FeatureEnums.MIN_EGO_DEGS], [FeatureEnums.MAX_EGO_DEGS], [FeatureEnums.RANGE_EGO_DEGS], [FeatureEnums.SKEWNESS_EGO_DEGS], [FeatureEnums.KURTOSIS_EGO_DEGS],  # Statistical features on degrees
                       #[FeatureEnums.ASSORTATIVITY_EGO], [FeatureEnums.INTERNAL_FRAC_EGO],  # Other features
                       #[FeatureEnums.CLOSENESS_CENTRALITY], [FeatureEnums.DEGREE_CENTRALITY], [FeatureEnums.EIGENVECTOR_CENTRALITY], [FeatureEnums.PAGERANK],  # [Feature.KATZ_CENTRALITY], Centrality measures
                       #[FeatureEnums.DEG, FeatureEnums.CLUSTER, FeatureEnums.AVG_EGO_DEG, FeatureEnums.AVG_EGO_CLUSTER] # Fugal
@@ -181,7 +180,7 @@ def tuning():
 
     run = list(range(len(tmp[1])))
 
-    iters = 1
+    iters = 5
 
     graph_names = [
         #"bio-celegans",
@@ -227,11 +226,11 @@ def tuning():
 
     noises = [
         0.00,
-        #0.05,
-        #0.10,
-        #0.15,
-        #0.20,
-        #0.25,
+        0.05,
+        0.10,
+        0.15,
+        0.20,
+        0.25,
     ]
     # s_trans = (2, 1, 0, 3)
     # s_trans = (0, 2, 1, 3,4)
